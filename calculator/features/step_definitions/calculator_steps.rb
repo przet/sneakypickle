a62bf04558fe061e8390dfea0399468eac0cd692
CrazyPenguin <p.rzetelski.note@gmail.com>
2017-10-20 09:55:12 +1030

    First commit of all initial cucumber work done on CrazyPenguin up until the date of this commit

---
+++ calculator/features/step_definitions/calculator_steps.rb
@@ -0,0 +1,14 @@
+
+
+Given(/^the input "([^"]*)"$/) do |input|
+ @input = input
+end
+
+When(/^the calculator is run$/) do
+ @output = `ruby calc.rb #{@input}`
+ raise('Command failed!') unless $?.success?
+end
+
+Then(/^the output should be "([^"]*)"$/) do |expected_output|
+ raise('Command failed!') unless $?.expect(@output).to eq expected_output?
+end
